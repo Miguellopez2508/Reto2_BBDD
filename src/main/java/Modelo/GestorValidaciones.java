f33e880f74daac5b862b08f36dc2c49b64cfda97
IN1DM3B_01 <IN1DM3B_01@H015112.NET>
2019-11-05 09:25:58 +0100

    acabadas validaciones empleados

--- src/main/java/Modelo/GestorValidaciones.java
+++ src/main/java/Modelo/GestorValidaciones.java
@@ -6,11 +6,48 @@ import Controlador.Main;
 public class GestorValidaciones {
 
 	public boolean validarEmpleado(EmpleadoControlador panelEmple) {
+		String pattern = "[0-9]+";
+		
 		if(panelEmple.tf1.getText().isEmpty()) {
 			Main.controlador.mostrarMensaje(panelEmple.panel, "Campo 'Nombre' vacío.");
 			return false;
 		}
-		
+		if(panelEmple.tf1.getText().length() < 6 || panelEmple.tf1.getText().length() > 20) {
+			Main.controlador.mostrarMensaje(panelEmple.panel, "Campo 'Nombre' debe medir entre 6 y 20 c.");
+			return false;
+		}
+		if(panelEmple.tf2.getText().isEmpty()) {
+			Main.controlador.mostrarMensaje(panelEmple.panel, "Campo 'Apellido' vacío.");
+			return false;
+		}
+		if(panelEmple.tf2.getText().length() < 6 || panelEmple.tf1.getText().length() > 20) {
+			Main.controlador.mostrarMensaje(panelEmple.panel, "Campo 'Apellido' debe medir entre 6 y 20 c.");
+			return false;
+		}
+		if(panelEmple.tf3.getText().isEmpty()) {
+			Main.controlador.mostrarMensaje(panelEmple.panel, "Campo 'Responsabilidad' vacío.");
+			return false;
+		}
+		if(panelEmple.tf3.getText().length() < 6 || panelEmple.tf1.getText().length() > 20) {
+			Main.controlador.mostrarMensaje(panelEmple.panel, "Campo 'Responsabilidad' debe medir entre 6 y 20 c.");
+			return false;
+		}
+		if(panelEmple.tf4.getText().isEmpty()) {
+			Main.controlador.mostrarMensaje(panelEmple.panel, "Campo 'Salario' vacío.");
+			return false;
+		}
+		if(!panelEmple.tf4.getText().matches(pattern)) {
+			Main.controlador.mostrarMensaje(panelEmple.panel, "Campo 'Salario' debe ser un número no decimal");
+			return false;
+		}
+		if(panelEmple.combo1.getSelectionModel().getSelectedItem() == null) {
+			Main.controlador.mostrarMensaje(panelEmple.panel, "Seleccione un gerente");
+			return false;
+		}
+		if(panelEmple.combo2.getSelectionModel().getSelectedItem() == null) {
+			Main.controlador.mostrarMensaje(panelEmple.panel, "Seleccione un departamaneto");
+			return false;
+		}
 		return true;
 	}
 	
